4cd9b3e22c54736c054c4aa962a4f910dc50fa00
Alberto Hermosillo <albertoherms17@gmail.com>
2024-03-14 20:27:39 -0600

    feature: install vuejs and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
+    darkMode: false,
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
